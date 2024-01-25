8a2760aa8d09eac9334e418f80cc338f6340deca
thmasq <thomasqsa@gmail.com>
2024-01-24 21:51:55 -0300

    fix(build): linting error stops compilation

--- src/components/OpenStreetMap.tsx
+++ src/components/OpenStreetMap.tsx
@@ -1,25 +1,17 @@
 "use client";
 
-import React, { useState, useRef } from "react";
-import { MapContainer, TileLayer, Marker, Popup, LayersControl, AttributionControl, GeoJSON } from "react-leaflet";
+import React, { useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup, LayersControl, AttributionControl, GeoJSON, GeoJSONProps, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { LatLngTuple } from "leaflet";
 import L from "leaflet";
 import riverData from './ne_10m_rivers_lake_centerlines.json';
-
 import icon from "leaflet/dist/images/marker-icon.png";
 import iconShadow from "leaflet/dist/images/marker-shadow.png";
-
-const DefaultIcon = L.icon({
-	iconUrl: icon,
-	shadowUrl: iconShadow,
-	iconAnchor: [12.5, 41], // Middle-bottom
-});
 
 const OpenStreetMap: React.FC = () => {
 	const [center, setCenter] = useState<LatLngTuple>([-15.793889, -47.882778]);
 	const ZOOM_LEVEL: number = 9;
-	const mapRef = useRef<HTMLDivElement | null>(null);
 
 	const onEachFeature = (feature: any, layer: L.GeoJSON) => {
 		if (feature.properties && feature.properties.name) {
@@ -33,7 +25,6 @@ const OpenStreetMap: React.FC = () => {
 			zoom={ZOOM_LEVEL}
 			attributionControl={false}
 			style={{ height: "100vh", width: "100vw" }}
-			ref={mapRef}
 		>
 			<LayersControl position="topright">
 				<LayersControl.BaseLayer checked name="OpenStreetMap">
@@ -56,7 +47,7 @@ const OpenStreetMap: React.FC = () => {
 				</LayersControl.BaseLayer>
 			</LayersControl>
 			<AttributionControl prefix='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' />
-        	<GeoJSON data={riverData} onEachFeature={onEachFeature} />
+			<GeoJSON data={riverData as GeoJSON.FeatureCollection} onEachFeature={onEachFeature} />
 		</MapContainer>
 	);
 };
